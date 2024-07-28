900ebf74378b8b7855ca4947179b4f381dc49495
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2024-07-28 23:33:33 +0530

    Database & Models Setup

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'creative_studio', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
